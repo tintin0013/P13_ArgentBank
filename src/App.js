67246911f6ef1fd8277b14df1cbb9b969d4436d4
tintin0013 <tintin0013@gmail.com>
2023-01-30 19:53:04 +0100

    mise en place du footer, de la page home et de la page login ainsi que du css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../src/pages/Home";
+import Login from "../src/pages/Login";
+
+import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        </Routes>
+        </BrowserRouter>
     </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //    <p>banane</p>
+    //   </header>
+    // </div>
   );
 }
 
